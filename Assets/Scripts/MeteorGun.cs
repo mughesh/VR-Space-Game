effb0a9e68c56131643a6d49bed4fb63ab9fa73e
Mugesh <mugheshambani@gmail.com>
2024-08-19 18:04:52 +0530

    update - Gun beam breaking meteor

--- Assets/Scripts/MeteorGun.cs
+++ Assets/Scripts/MeteorGun.cs
@@ -6,6 +6,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class MeteorGun : MonoBehaviour
 {   
     public ParticleSystem particles;
+    public LayerMask layerMask;
+    public Transform shootSource;
+    public float distance = 10;
+    private bool gunActivated = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +23,33 @@ public class MeteorGun : MonoBehaviour
     public void startShoot()
     {
         particles.Play();
-        Debug.Log("Trigger button pressed");
+        gunActivated = true;
+        //Debug.Log("Trigger button pressed");
     }
 
     public void stopShoot()
     {
         particles.Stop(true,ParticleSystemStopBehavior.StopEmittingAndClear);
+        gunActivated = false;
+    }
+
+    void RaycastHitCheck()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(shootSource.position,shootSource.forward,out hit,distance,layerMask);
+        if (hasHit)
+        {
+            hit.transform.gameObject.SendMessage("Break",SendMessageOptions.DontRequireReceiver);
+            Debug.Log("raycast Hit !!!");
+        }
     }
     // Update is called once per frame
     void Update()
     {
+        if(gunActivated)
+        {
+            RaycastHitCheck();
+        }
         
     }
 }
